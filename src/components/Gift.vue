f77289805c711a94c300a3f2e749d91dfced9254
maraisaferreira <ferreira.maraisacristina@gmail.com>
2021-02-15 17:26:59 -0300

    comp. gift implementado

---
+++ src/components/Gift.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="gift">
+      <img src="../assets/imgs/gift.gif">
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'Gift'
+}
+</script>
+
+<style>
+ .gift {
+     position: relative;
+     display: flex;
+     align-items: center;
+     justify-content: center;
+     z-index: 10;
+     border: 1px solid red;
+ }
+
+ .gift img {
+     width: 100px;
+     height: 95px;
+ }
+</style>
