26463dd878baaaa65302f75e0f4318361aa4f3be
maraisaferreira <ferreira.maraisacristina@gmail.com>
2021-02-16 10:14:19 -0300

    door layout fibalizado

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,43 @@
 <template>
-  <div id="app">
-      <h1>Monty Hall</h1>
-      <Gift />
-      <Error />
-  </div>
+	<div id="app">
+		<h1>Monty Hall</h1>
+		<Door number="1" :hasGift="false" />
+	</div>
 </template>
 
 <script>
-import Gift from './components/Gift'
-import Error from './components/Error'
+import Door from './components/Door';
 
 export default {
-  name: 'App',
-  components: { Gift, Error }
-}
+	name: 'App',
+	components: { Door },
+};
 </script>
 
 <style>
-*{
-  box-sizing: border-box;
-  font-family: 'Montserrat', sans-serif;
+* {
+	box-sizing: border-box;
+	font-family: 'Montserrat', sans-serif;
 }
 
 body {
-  color: white;
-  background: #121FCF;
-  background: linear-gradient(to top right, #121FCF 0%, #ec06d9 89%);
+	color: white;
+	background: #121fcf;
+	background: linear-gradient(to top right, #121fcf 0%, #ec06d9 89%);
 }
 
 #app {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	height: 100vh;
 }
 
 #app h1 {
-  text-shadow: 4px 2px 3px rgba(0, 0, 0, 0.68);
-  background: #0008;
-  padding: 20px;
-  margin-bottom: 60px;
-  border: 1px solid black;
+	text-shadow: 4px 2px 3px rgba(0, 0, 0, 0.68);
+	background: #0008;
+	padding: 20px;
+	margin-bottom: 60px;
+	border: 1px solid black;
 }
-
 </style>
---
+++ src/components/Door.vue
@@ -0,0 +1,116 @@
+<template>
+	<div class="door-area">
+		<div class="door-frame">
+			<Gift v-if="open && hasGift" />
+			<Error v-if="open && !hasGift" />
+		</div>
+		<div class="door" @click="selected = !selected" :class="{ selected, open }">
+			<div class="img-box">
+				<img src="../assets/imgs/door.jpg" />
+			</div>
+			<div class="number">{{ number }}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Gift from './Gift';
+import Error from './Error';
+
+export default {
+	name: 'Door',
+	components: { Gift, Error },
+	props: {
+		number: {},
+		hasGift: { type: Boolean },
+	},
+
+	data() {
+		return {
+			open: false,
+			selected: false,
+		};
+	},
+};
+</script>
+
+<style>
+:root {
+	--border-frame: solid 11px #c7660ce0;
+}
+
+.door-area {
+	position: relative;
+	width: 200px;
+	height: 310px;
+	margin-bottom: 20px;
+	display: flex;
+	justify-content: center;
+	font-size: 1.3rem;
+}
+
+.door-frame {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: flex-end;
+
+	border-top: var(--border-frame);
+	border-right: var(--border-frame);
+	border-left: var(--border-frame);
+	background: black;
+}
+
+.door {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.img-box {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+}
+
+.img-box img {
+	width: 100%;
+	height: 100%;
+}
+
+.door .number {
+	position: absolute;
+	z-index: 10;
+	margin-top: 69px;
+	width: 30px;
+	border: 1px solid black;
+	background: #0009;
+	border-radius: 5px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.door.selected {
+	box-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #ccff00,
+		0 0 20px #ccff00, 0 0 20px #ccff00, 0 0 25px #ccff00, 0 0 20px #ccff00;
+}
+
+.door.open {
+	display: none;
+}
+
+.door.open .img-box {
+	display: none;
+}
+
+.door.open .number {
+	display: none;
+}
+</style>
